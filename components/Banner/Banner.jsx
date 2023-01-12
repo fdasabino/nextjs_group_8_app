205cc399344c962a05071454b891a275a82cfa31
Francisco Sabino <fdasabino@gmail.com>
2023-01-12 16:41:36 +0100

    FEATURE: Banner component created
    Co-authored-by: Regan Ya <hens.virtuella.varld@gmail.com>
    Co-authored-by: Sammani Ariyarathne <SammaniAri@users.noreply.github.com>

---
+++ components/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+
+const HEADING_NAME = {
+  gallery: "NFT GALLERY",
+  marketPlace: "OUR MARKETPLACE",
+};
+
+const Banner = ({ headingType }) => {
+  const [heading] = useTypewriter({
+    words: [HEADING_NAME[headingType]],
+    loop: true,
+    delaySpeed: 1000,
+    typeSpeed: 100,
+  });
+
+  return (
+    <div className="banner">
+      <h2>{heading}</h2>
+      <Cursor cursorColor="white" cursorBlinking={false} />
+    </div>
+  );
+};
+
+export default Banner;
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import "../styles/globals.scss";
 import "../styles/Button.scss";
 import "../styles/Navbar.scss";
 import "../styles/Hero.scss";
+import "../styles/Banner.scss";
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
