8ac454168986d49e0ddcc5ccaaabd17a38017a57
Francisco Sabino <fdasabino@gmail.com>
2023-01-13 14:34:13 +0100

    updated font import

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Orbitron:regular,500,600,700,800,900&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
