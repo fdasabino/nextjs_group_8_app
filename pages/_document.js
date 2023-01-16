f98d70de8e994531c6454a83cab7fcccfafc6137
Francisco Sabino <fdasabino@gmail.com>
2023-01-16 11:00:58 +0100

    update font family on _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body>
+      <body style={{ fontFamily: "Orbitron" }}>
         <Main />
         <NextScript />
       </body>
